46bdf154f63b5df163212d018f73ab433f93990f
Kitamuro <33262734+Kitamuro@users.noreply.github.com>
2017-11-17 22:20:40 +0600

    Entity, operation, user
    Снова какая-то ошибка, вроде логика правильная.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,134 @@
 #include<iostream>
 using namespace std;
-class Array{
-    int *arr;
+
+class Entity {
+protected:
+    int id;
 public:
-    static int count;
-    Array(int i){
-        arr = new int[i];
-        count++;
-    }
-    Array(const Array & prevArray){
-        arr = prevArray.arr;
-    }
-    Array(){
+    Entity() {
         
     }
-    int getArray(){
-        return *arr;
+    explicit Entity(int id) {
+        this->id = id;
     }
-    void setArray(int *arr){
-        this->arr = arr;
+    Entity(const Entity &entity) {
+        setId(entity.id);
     }
-    ~Array(){
-        delete arr;
+    ~Entity() {
+        
+    }
+    int getId() {
+        return id;
+    }
+    void setId(int id) {
+        this->id = id;
     }
 };
-int main(){
-    Array *r1 = new Array(2);
-    Array *r2 = new Array(3);
-    cout<<r2->count<<endl;
-    delete r1;
-    delete r2;
+
+class Operation :public Entity {
+public:
+    Operation * next;
+    string name;
+    int timestamp;
+    explicit Operation(string name, int timestamp) {
+        this->name = name;
+        this->timestamp = timestamp;
+    }
+    Operation(const Operation &operat){
+        this->name = operat.name;
+        this->timestamp = operat.timestamp;
+    }
+    Operation() {
+        
+    }
+    ~Operation() {
+        delete next;
+    }
+};
+
+class User :public Entity {
+public:
+    string firstName, secondName;
+    Operation *firstoperation, *lastoperation;
+    Entity *op;
+    
+    explicit User(string firstName, string secondName) {
+        this->firstName = firstName;
+        this->secondName = secondName;
+    }
+    User(){
+        
+    }
+    ~User() {
+        delete firstoperation;
+        delete lastoperation;
+        delete op;
+    }
+    
+    void addOperation(string name, int timestamp) {
+        Operation *temp = new Operation(name, timestamp);
+        if (!firstoperation) {
+            firstoperation = temp;
+            lastoperation = temp;
+        }
+        else {
+            lastoperation->next = temp;
+            lastoperation = temp;
+        }
+    }
+    void readOperation() {
+        Operation *temp = firstoperation;
+        while (temp) {
+            cout <<"id: "<<op->getId()<< "timestamp: " << temp->timestamp << "name: " << temp->name << endl;
+            temp = temp->next;
+        }
+    }
+    void addUser(){
+        string firstName, lastName;
+        int c = 10000;
+        cout<<"Input your first and last name: "<<endl;
+        cin>>firstName>>lastName;
+        this->firstName = firstName;
+        this->secondName = lastName;
+        op->setId(c);
+        c++;
+    }
+};
+int main()
+{
+    User *user1 = new User[100];
+    int key;
+    bool check = true;
+    int i = 0, k = 0;
+    cout << "Enter 1-to add user, 2-to add operation, 3-show operation" << endl;
+    cin>>key;
+    while(check){
+    switch(key){
+        case 1:{
+            user1[i].addUser();
+            i++;
+            cout<<"Your id is: "<<user1[i].getId()<<endl;
+        }
+        case 2:{
+            cout<<"Input name and time(UNIX)"<<endl;
+            user1[i].readOperation();
+            i++;
+        }
+        case 3:{
+            cout<<"Input your id: "<<endl;
+            cin>>k;
+            for(int j = 0 ; j < i; j++){
+                if(k == user1[i].getId()){
+                    user1[j].readOperation();
+                }
+            }
+        }
+        case 4:
+            check = true;
+        default:
+            break;
+    }
+    }
+    delete [] user1;
 }
 
